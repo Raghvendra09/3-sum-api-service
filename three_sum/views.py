903f53bc6d9b89d3e8d58ade0e204ec30b16f110
Raghvendra Singh <raghvendrasingh@rdskytech.com>
2020-08-04 00:16:57 +0530

    Updated logic and improved space complexity from O(N) to O(1).

--- three_sum/views.py
+++ three_sum/views.py
@@ -7,32 +7,35 @@ from three_sum.serializers import ThreeSumSerializer, TransactionSerializer
 from three_sum.models import Transactions
 import json
 import datetime
-# Create your views here.
 
 
 class Solution:
     def __init__(self, input_list, target):
-        self.input_list = input_list
+        self.input_list = sorted(input_list)
         self.target = target
+        self.ans = []
 
     def three_sum_solution(self):
-        for num in self.input_list:
-            self.target = self.target - num
-            ans = self.__two_sum_solution()
-            ans = [[num] + i for i in ans]
-            return ans
+        for position, num in enumerate(self.input_list):
+            if (num == 0 or self.input_list[position] != self.input_list[position-1]) and self.input_list[position] <= 0:
+                self.__two_sum_solution(position)
+        return self.ans
 
-    def __two_sum_solution(self): # Incapsulation
-        nums = self.input_list
-        seen_numbers = []
-        ans = []
-        for i in nums:
-            remaining = self.target - i
-            if remaining in seen_numbers:
-                ans.append([remaining, i])
+    def __two_sum_solution(self, position):
+
+        target = self.target - self.input_list[position]
+        low, high = position+1, len(self.input_list) - 1
+        while low < high:
+            sum = self.input_list[low] + self.input_list[high]
+            if sum < target or (low > high and self.input_list[low] == self.input_list[low-1]):
+                low += 1
+            elif sum > target or (high < len(self.input_list) -1 and self.input_list[high] == self.input_list[high+1]):
+                high -= 1
             else:
-                seen_numbers.append(i)
-        return ans
+                self.ans.append([self.input_list[position], self.input_list[low], self.input_list[high]])
+                low += 1
+                high -= 1
+        return
 
 
 class ThreeSum(APIView):
